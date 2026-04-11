66556a06f309f3d6d77b8b286d05889d61061a72
shinjuyongg <214128079+juyong04@users.noreply.github.com>
2026-04-11 16:11:48 +0900

    firebase config 연결

---
+++ senior-app/src/firebase/config.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBVa43WWRo449K5m6g0MSuO1ZRDDB9_-_U",
+  authDomain: "capstone2-a7107.firebaseapp.com",
+  projectId: "capstone2-a7107",
+  storageBucket: "capstone2-a7107.firebasestorage.app",
+  messagingSenderId: "250261611850",
+  appId: "1:250261611850:web:730ea8d8134f93306d8db3",
+  measurementId: "G-8QBGN7F4W4"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
